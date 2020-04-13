fea8e864700fc8983654b28722f529770118027a
Anthony Richir <anthony.richir@gmail.com>
2020-04-13 23:41:41 +0200

    Exercise 14

--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -1,5 +1,7 @@
 package com.lightbend.training.scalatrain
 
-case class Train(kind: String, number: Int, schedule: Seq[Station]) {
+case class Train(kind: String, number: Int, schedule: Seq[(Time,Station)]) {
   require(schedule.size >= 2, "Schedule have at least 2 elements")
+
+  val stations = schedule.map(_._2)
 }
