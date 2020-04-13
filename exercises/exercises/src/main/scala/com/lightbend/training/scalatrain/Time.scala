c569f6efae6914d6532a1ca9cbc4668859f03bf1
Anthony Richir <arichir@altissia.com>
2020-04-13 10:20:10 +0200

    Exercise 10

--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
@@ -4,9 +4,19 @@ class Time(val hours: Int = 0, val minutes: Int = 0) {
   // TODO Verify that hours is within 0 and 23
   // TODO Verify that minutes is within 0 and 59
 
+  require(hours >= 0 && hours <= 23, "hours must be within 0 and 23")
+  require(minutes >= 0 && minutes <= 59, "minutes must be within 0 and 59")
+
   val asMinutes: Int = hours * 60 + minutes
 
   def minus(that: Time): Int = asMinutes - that.asMinutes
 
   def -(that: Time): Int = minus(that)
+}
+
+object Time {
+  def fromMinutes(minutes: Int): Time = {
+    val hours = minutes / 60
+    new Time(hours, minutes % 60)
+  }
 }
