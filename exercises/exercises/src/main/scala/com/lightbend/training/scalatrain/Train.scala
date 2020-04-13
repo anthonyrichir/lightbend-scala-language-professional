2d0dedec52ce6521c127d6d96016222553c6fa5f
Anthony Richir <anthony.richir@gmail.com>
2020-04-13 22:41:21 +0200

    Exercise 13

--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -1,3 +1,5 @@
 package com.lightbend.training.scalatrain
 
-case class Train(kind: String, number: Int)
+case class Train(kind: String, number: Int, schedule: Seq[Station]) {
+  require(schedule.size >= 2, "Schedule have at least 2 elements")
+}
