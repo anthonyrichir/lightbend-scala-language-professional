effbbf0b5316cddb873855ba678312d4425f2fa3
Anthony Richir <arichir@altissia.com>
2020-04-15 08:11:50 +0200

    Exercise 16

--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
@@ -3,4 +3,6 @@ package com.lightbend.training.scalatrain
 class JourneyPlanner(val trains: Set[Train]) {
 
     val stations: Set[Station] = trains.flatMap(train => train.stations)
+
+    def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
 }
