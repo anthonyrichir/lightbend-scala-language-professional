d39da0ed2a78db6a857ae4afef2eea7efc47f1c3
Anthony Richir <anthony.richir@gmail.com>
2020-04-15 22:33:35 +0200

    Exercise 17

--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
@@ -5,4 +5,11 @@ class JourneyPlanner(val trains: Set[Train]) {
     val stations: Set[Station] = trains.flatMap(train => train.stations)
 
     def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
+
+    def stopsAt(station: Station): Set[(Time,Train)] = {
+        for {
+            train <- trains
+            timeAndStation <- train.schedule if timeAndStation._2 == station
+        } yield timeAndStation._1 -> train
+    }
 }
