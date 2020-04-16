30181b8ca1fc16ebb79c55d8d952b70d3230a2d9
Anthony <anthony.richir@gmail.com>
2020-04-16 12:07:41 +0200

    Exercise 21

--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
@@ -2,14 +2,26 @@ package com.lightbend.training.scalatrain
 
 class JourneyPlanner(val trains: Set[Train]) {
 
-    val stations: Set[Station] = trains.flatMap(train => train.stations)
+  val stations: Set[Station] = trains.flatMap(train => train.stations)
 
-    def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
+  def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
 
-    def stopsAt(station: Station): Set[(Time,Train)] = {
-        for {
-            train <- trains
-            timeAndStation <- train.schedule if timeAndStation._2 == station
-        } yield timeAndStation._1 -> train
-    }
+  def stopsAt(station: Station): Set[(Time, Train)] = {
+    for {
+      train <- trains
+      timeAndStation <- train.schedule if timeAndStation._2 == station
+    } yield timeAndStation._1 -> train
+  }
+
+  def isShortTrip(from: Station, to: Station): Boolean = {
+    //    trains.exists(train =>
+    //      train.stations.dropWhile(station => station != from).drop(1).take(2).contains(to)
+    //    )
+
+    trains.exists(train => train.stations.dropWhile(station => station != from) match {
+      case `from` +: `to` +: _ => true
+      case `from` +: _ +: `to` +: _ => true
+      case _ => false
+    })
+  }
 }
