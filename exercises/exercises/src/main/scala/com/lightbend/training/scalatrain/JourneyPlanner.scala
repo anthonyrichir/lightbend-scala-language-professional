37c0bac22ade0e879511c7142d83ccde247f50b7
Anthony <anthony.richir@gmail.com>
2020-04-16 14:18:49 +0200

    Exercise 23

--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
@@ -7,12 +7,16 @@ class JourneyPlanner(val trains: Set[Train]) {
   def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
 
   def stopsAt(station: Station): Set[(Time, Train)] = {
+    //    for {
+    //      train <- trains
+    ////      timeAndStation <- train.schedule if timeAndStation._2 == station
+    ////    } yield timeAndStation._1 -> train
+    //      (t, s) <- train.schedule if s == station
+    //    } yield t -> train
     for {
       train <- trains
-//      timeAndStation <- train.schedule if timeAndStation._2 == station
-//    } yield timeAndStation._1 -> train
-      (t, s) <- train.schedule if s == station
-    } yield t -> train
+      time <- train.timeAt(station)
+    } yield time -> train
   }
 
   def isShortTrip(from: Station, to: Station): Boolean = {
--- exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -1,7 +1,16 @@
 package com.lightbend.training.scalatrain
 
-case class Train(info: TrainInfo, schedule: Seq[(Time,Station)]) {
+case class Train(info: TrainInfo, schedule: Seq[(Time, Station)]) {
   require(schedule.size >= 2, "Schedule have at least 2 elements")
 
   val stations = schedule.map(_._2)
+
+  def timeAt(station: Station): Option[Time] = {
+    //    schedule.find(timeAndStation => timeAndStation._2 == station).map(timeAndStation => timeAndStation._1)
+    schedule.find {
+      case (_, needle) => needle == station
+    }.map {
+      case (time, _) => time
+    }
+  }
 }
